a6507b803324a64772a3e148090ca8609836a163
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-05-30 00:39:33 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,57 +5,57 @@ export const TECH_SKILLS = [
     },
     {
         "name": "React",
-        "logo": "/public/svg/symbol-defs.svg#icon-react"
+        "logo": "/svg/symbol-defs.svg#icon-react"
     },
     {
         "name": "Node JS",
-        "logo": "/public/svg/symbol-defs.svg#icon-NodeJs"
+        "logo": "/svg/symbol-defs.svg#icon-NodeJs"
     },
     {
         "name": "HTML5",
-        "logo": "/public/svg/symbol-defs.svg#icon-w3_html5-icon"
+        "logo": "/svg/symbol-defs.svg#icon-w3_html5-icon"
     },
     {
         "name": "CSS3",
-        "logo": "/public/svg/symbol-defs.svg#icon-w3_css-iconold"
+        "logo": "/svg/symbol-defs.svg#icon-w3_css-iconold"
     },
     {
         "name": "React Router",
-        "logo": "/public/svg/symbol-defs.svg#icon-reactrouter-icon"
+        "logo": "/svg/symbol-defs.svg#icon-reactrouter-icon"
     },
     {
         "name": "Redux",
-        "logo": "/public/svg/symbol-defs.svg#icon-js_redux-icon"
+        "logo": "/svg/symbol-defs.svg#icon-js_redux-icon"
     },
     {
         "name": "TypeScript",
-        "logo": "/public/svg/symbol-defs.svg#icon-typescriptlang-icon"
+        "logo": "/svg/symbol-defs.svg#icon-typescriptlang-icon"
     }
 ];
 
 export const TOOLS = [
     {
         "name": "Figma",
-        "logo": "/public/svg/symbol-defs.svg#icon-figma-icon"
+        "logo": "/svg/symbol-defs.svg#icon-figma-icon"
     },
     {
         "name": "Postman",
-        "logo": "/public/svg/symbol-defs.svg#icon-getpostman-icon"
+        "logo": "/svg/symbol-defs.svg#icon-getpostman-icon"
     },
     {
         "name": "Vercel",
-        "logo": "/public/svg/symbol-defs.svg#icon-vercel-icon"
+        "logo": "/svg/symbol-defs.svg#icon-vercel-icon"
     },
     {
         "name": "Render",
-        "logo": "/public/svg/symbol-defs.svg#icon-render"
+        "logo": "/svg/symbol-defs.svg#icon-render"
     },
     {
         "name": "Swagger",
-        "logo": "/public/svg/symbol-defs.svg#icon-swagger"
+        "logo": "/svg/symbol-defs.svg#icon-swagger"
     },
     {
         "name": "Mongo DB",
-        "logo": "/public/svg/symbol-defs.svg#icon-mongodb-icon"
+        "logo": "/svg/symbol-defs.svg#icon-mongodb-icon"
     }
 ];
